634dbe650b17ab304d4ab6bf3fbcdc0b820de909
Itas666 <ia666@live.mdx.ac.uk>
2022-11-17 23:53:08 +0000

    Re-sending data to master branch

--- Courseworks/products.js
+++ Courseworks/products.js
@@ -1,12 +1,12 @@
 let products = [
     {name: 'Math', price: 50, availability: 5, image: 'https://www.mathsisfun.com/numbers/images/number-line-0-100.svg', description: 'Math is a subject', location: 'London', icon: 'fa fa-calculator'},
-    {name: 'English', price: 40, availability: 10, image: 'https://www.englishclub.com/images/vocabulary/letters/letter-a.jpg', description: 'English is a subject', location: 'London', icon: 'fa fa-language'},
-    {name: 'Science', price: 30, availability: 15, image: 'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__340.jpg', description: 'Science is a subject, but also an art', location: 'London', icon: 'fa fa-flask'},
-    {name: 'History', price: 20, availability: 20, image: 'https://www.history.com/.image/t_share/MTU3NjM4MjQwNjQ5MjQyNjQy/history-logo.jpg', description: 'History is a subject', location: 'London', icon: 'fa fa-history'},
-    {name: 'Geography', price: 10, availability: 25, image: 'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__340.jpg', description: 'Geography is a subject', location: 'London', icon: 'fa fa-globe'},
-    {name: 'Art', price: 5, availability: 30, image: 'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__340.jpg', description: 'Art is a subject, all heart no science', location: 'London', icon: 'fa fa-paint-brush'},
-    {name: 'Music', price: 5, availability: 35, image: 'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__340.jpg', description: 'Music is a subject', location: 'London', icon: 'fa fa-music'},
-    {name: 'Spanish', price: 5, availability: 40, image: 'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__340.jpg', description: 'Spanish is a subject', location: 'London', icon: 'fa fa-language'},
-    {name: 'Drama', price: 5, availability: 45, image: 'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__340.jpg', description: 'Drama is a subject', location: 'London', icon: 'fa fa-film'},
-    {name: 'French', price: 5, availability: 50, image: 'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__340.jpg', description: 'French is a subject', location: 'London', icon: 'fa fa-language'}
+    {name: 'English', price: 40, availability: 5, image: 'https://www.englishclub.com/images/vocabulary/letters/letter-a.jpg', description: 'English is a subject', location: 'London', icon: 'fa fa-language'},
+    {name: 'Science', price: 30, availability: 5, image: 'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__340.jpg', description: 'Science is a subject, but also an art', location: 'London', icon: 'fa fa-flask'},
+    {name: 'History', price: 20, availability: 5, image: 'https://www.history.com/.image/t_share/MTU3NjM4MjQwNjQ5MjQyNjQy/history-logo.jpg', description: 'History is a subject', location: 'London', icon: 'fa fa-history'},
+    {name: 'Geography', price: 10, availability: 5, image: 'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__340.jpg', description: 'Geography is a subject', location: 'London', icon: 'fa fa-globe'},
+    {name: 'Art', price: 5, availability: 5, image: 'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__340.jpg', description: 'Art is a subject, all heart no science', location: 'London', icon: 'fa fa-paint-brush'},
+    {name: 'Music', price: 5, availability: 5, image: 'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__340.jpg', description: 'Music is a subject', location: 'London', icon: 'fa fa-music'},
+    {name: 'Spanish', price: 5, availability: 5, image: 'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__340.jpg', description: 'Spanish is a subject', location: 'London', icon: 'fa fa-language'},
+    {name: 'Drama', price: 5, availability: 5, image: 'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__340.jpg', description: 'Drama is a subject', location: 'London', icon: 'fa fa-film'},
+    {name: 'French', price: 5, availability: 5, image: 'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__340.jpg', description: 'French is a subject', location: 'London', icon: 'fa fa-language'}
 ];
